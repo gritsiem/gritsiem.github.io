9089d3910d5f6c2c1fdd7527c3db549229a1460e
gritsiem <46785182+gritsiem@users.noreply.github.com>
2025-12-21 21:58:55 +0000

    Deploying to gh-pages from @ gritsiem/gritsiem.github.io@f5990fc866e559984c1b39f4d4cf3d1ea8c97151 🚀

---
+++ assets/js/search-data.js
@@ -0,0 +1,125 @@
+// get the ninja-keys element
+const ninja = document.querySelector('ninja-keys');
+
+// add the home and posts menu items
+ninja.data = [{
+    id: "nav-about",
+    title: "About",
+    section: "Navigation",
+    handler: () => {
+      window.location.href = "/";
+    },
+  },{id: "nav-projects",
+          title: "Projects",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/projects/";
+          },
+        },{id: "nav-repositories",
+          title: "Repositories",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/repositories/";
+          },
+        },{id: "nav-cv",
+          title: "CV",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/cv/";
+          },
+        },{id: "books-the-godfather",
+          title: 'The Godfather',
+          description: "",
+          section: "Books",handler: () => {
+              window.location.href = "/books/the_godfather/";
+            },},{id: "news-a-simple-inline-announcement",
+          title: 'A simple inline announcement.',
+          description: "",
+          section: "News",},{id: "news-a-long-announcement-with-details",
+          title: 'A long announcement with details',
+          description: "",
+          section: "News",handler: () => {
+              window.location.href = "/news/announcement_2/";
+            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
+          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+          description: "",
+          section: "News",},{id: "projects-cloud-logging-with-ai-insight",
+          title: 'Cloud Logging with AI Insight',
+          description: "Uses the ElasticSearch, Logstash and Kibana stack to store logs, but with an LLM component to generate possible debugging insights.",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/ailog/";
+            },},{id: "projects-email-labelling",
+          title: 'Email Labelling',
+          description: "A live project for me to organize my emails and do more analysis on them",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/emaillab/";
+            },},{id: "projects-image-captioning",
+          title: 'Image Captioning',
+          description: "A Pre-LLM era image captioning model finetuning project!",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/imcap/";
+            },},{
+        id: 'social-email',
+        title: 'email',
+        section: 'Socials',
+        handler: () => {
+          window.open("mailto:%67%68%72%69%74%61%63%68%69%6D%61%68%61%6A%61%6E%69%35+%70%6F%72%74%66%6F%6C%69%6F@%67%6D%61%69%6C.%63%6F%6D", "_blank");
+        },
+      },{
+        id: 'social-cv',
+        title: 'CV',
+        section: 'Socials',
+        handler: () => {
+          window.open("/assets/pdf/ghritachi_mahajani_resume.pdf", "_blank");
+        },
+      },{
+        id: 'social-linkedin',
+        title: 'LinkedIn',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://www.linkedin.com/in/ghritachi-mahajani", "_blank");
+        },
+      },{
+        id: 'social-github',
+        title: 'GitHub',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://github.com/gritsiem", "_blank");
+        },
+      },{
+        id: 'social-scholar',
+        title: 'Google Scholar',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://scholar.google.com/citations?user=v-PY11YAAAAJ", "_blank");
+        },
+      },{
+      id: 'light-theme',
+      title: 'Change theme to light',
+      description: 'Change the theme of the site to Light',
+      section: 'Theme',
+      handler: () => {
+        setThemeSetting("light");
+      },
+    },
+    {
+      id: 'dark-theme',
+      title: 'Change theme to dark',
+      description: 'Change the theme of the site to Dark',
+      section: 'Theme',
+      handler: () => {
+        setThemeSetting("dark");
+      },
+    },
+    {
+      id: 'system-theme',
+      title: 'Use system default theme',
+      description: 'Change the theme of the site to System Default',
+      section: 'Theme',
+      handler: () => {
+        setThemeSetting("system");
+      },
+    },];
